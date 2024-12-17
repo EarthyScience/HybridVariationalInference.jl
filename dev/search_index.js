8c069e88029b750432d7f511199aec19ce27a58c
Documenter.jl <documenter@juliadocs.github.io>
2024-12-17 10:49:37 +0000

    build based on fc4fe0b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HybridVariationalInference","category":"page"},{"location":"#HybridVariationalInference","page":"Home","title":"HybridVariationalInference","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HybridVariationalInference.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HybridVariationalInference]","category":"page"}]
+}
