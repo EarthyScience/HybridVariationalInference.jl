28d54e383c48dd8ffeb49e83db08c918feb9cb41
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2025-02-28 18:32:41 +0100

    test for function CUDA  in test

--- test/test_ModelApplicator.jl
+++ test/test_ModelApplicator.jl
@@ -7,16 +7,16 @@ using MLDataDevices, CUDA, cuDNN, GPUArraysCore
 
 @testset "NullModelApplicator" begin
     g = NullModelApplicator()
-    c1 = CA.ComponentVector(a=(a1=1,a2=2:3),b=3:4)
+    c1 = CA.ComponentVector(a = (a1 = 1, a2 = 2:3), b = 3:4)
     y = g(c1, nothing)
     @test y == c1
 end;
 
 @testset "MagnitudeModelApplicator" begin
     app = NullModelApplicator()
-    c1 = CA.ComponentVector(a=(a1=1,a2=2:3),b=3:4)
-    m = c1*2
-    g = MagnitudeModelApplicator(app, m)  
+    c1 = CA.ComponentVector(a = (a1 = 1, a2 = 2:3), b = 3:4)
+    m = c1 * 2
+    g = MagnitudeModelApplicator(app, m)
     y = g(c1, eltype(m)[])
     @test y == c1 .* m
 end;
@@ -26,16 +26,18 @@ end;
     r = logistic.(randn(5)) # 0..1
     σ = fill(2.0, 5)
     μ = collect(exp.(1.0:5.0)) # different magnitudes
-    g = NormalScalingModelApplicator(app, μ, σ)  
+    g = NormalScalingModelApplicator(app, μ, σ)
     y = g(r, eltype(μ)[])
     p = normcdf.(μ, σ, y)
     #hcat(r, p)
     @test p ≈ r
     gdev = gpu_device()
-    cdev = cpu_device()
-    g_gpu = g |> gdev
-    @test g_gpu.μ isa GPUArraysCore.AbstractGPUArray
-    r_gpu = r |> gdev
-    y = g_gpu(r_gpu, eltype(g_gpu.μ)[])
-    @test y isa GPUArraysCore.AbstractGPUArray
+    #cdev = cpu_device()
+    if MLDataDevices.functional(gdev)
+        g_gpu = g |> gdev
+        @test g_gpu.μ isa GPUArraysCore.AbstractGPUArray
+        r_gpu = r |> gdev
+        y = g_gpu(r_gpu, eltype(g_gpu.μ)[])
+        @test y isa GPUArraysCore.AbstractGPUArray
+    end
 end;
