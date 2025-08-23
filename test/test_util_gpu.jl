60c5c8860c192ddef28efa0f010d703d1481a577
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2025-08-23 17:32:30 +0200

    check for CUDA before calling CUDA

--- test/test_util_gpu.jl
+++ test/test_util_gpu.jl
@@ -1,18 +1,25 @@
 using Test
 using HybridVariationalInference: HybridVariationalInference as HVI
 using ComponentArrays
-using CUDA
+using MLDataDevices
+import CUDA, cuDNN
 using FillArrays
 
 @testset "ones_similar_x" begin
     A = rand(Float64, 3, 4); 
-    B = CUDA.rand(Float32, 5, 2);    # GPU matrix
     @test HVI.ones_similar_x(A, 3) isa FillArrays.AbstractFill #Vector
     @test HVI.ones_similar_x(A, size(A,1)) isa FillArrays.AbstractFill #Vector#Vector
-    @test HVI.ones_similar_x(B, size(B,1)) isa CuArray
-    @test HVI.ones_similar_x(ComponentVector(b=B), size(B,1)) isa CuArray
-    @test HVI.ones_similar_x(B', size(B,1)) isa CuArray
-    @test HVI.ones_similar_x(@view(B[:,2]), size(B,1)) isa CuArray
-    @test HVI.ones_similar_x(ComponentVector(b=B)[:,1], size(B,1)) isa CuArray
+end
+
+gdev = gpu_device()
+if gdev isa MLDataDevices.CUDADevice
+    @testset "ones_similar_x" begin
+        B = CUDA.rand(Float32, 5, 2);    # GPU matrix
+        @test HVI.ones_similar_x(B, size(B,1)) isa CuArray
+        @test HVI.ones_similar_x(ComponentVector(b=B), size(B,1)) isa CuArray
+        @test HVI.ones_similar_x(B', size(B,1)) isa CuArray
+        @test HVI.ones_similar_x(@view(B[:,2]), size(B,1)) isa CuArray
+        @test HVI.ones_similar_x(ComponentVector(b=B)[:,1], size(B,1)) isa CuArray
+    end
 end
 
